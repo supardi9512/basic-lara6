064eb708f14cdca149828a6d1eaaf36edae07c6e
Supardi <supardi9512@gmail.com>
2020-03-15 16:25:11 +0700

    route resource dan api resource

--- routes/web.php
+++ routes/web.php
@@ -16,68 +16,74 @@ Route::get('/', function () {
 });
 
 
-Route::get('/user', function() {
-    return 'User';
-});
+// Route::get('/user', function() {
+//     return 'User';
+// });
 
-// ROUTING DENGAN 1 PARAMETER
-Route::get('/user/{id}', function($id) {
-    return 'User '.$id;
-});
+// // ROUTING DENGAN 1 PARAMETER
+// Route::get('/user/{id}', function($id) {
+//     return 'User '.$id;
+// });
 
-// ROUTING DENGAN BANYAK PARAMETER
-Route::get('/user/{id}/article/{articleId}', function($id, $articleId) {
-    return 'User '.$id.' Artikel '.$articleId;
-});
+// // ROUTING DENGAN BANYAK PARAMETER
+// Route::get('/user/{id}/article/{articleId}', function($id, $articleId) {
+//     return 'User '.$id.' Artikel '.$articleId;
+// });
 
-// ROUTING DENGAN 1 PARAMETER OPSIONAL
-Route::get('/admin/{id?}', function($id = null) {
-    return 'Admin '.$id;
-});
+// // ROUTING DENGAN 1 PARAMETER OPSIONAL
+// Route::get('/admin/{id?}', function($id = null) {
+//     return 'Admin '.$id;
+// });
 
-// ROUTE GROUP
-Route::prefix('account')->group(function() {
-    // definisikan route
-    Route::get('/change-password', function() {
-        return 'change-password';
-    });
+// // ROUTE GROUP
+// Route::prefix('account')->group(function() {
+//     // definisikan route
+//     Route::get('/change-password', function() {
+//         return 'change-password';
+//     });
 
-    Route::get('/profile', function() {
-        return 'profile';
-    });
+//     Route::get('/profile', function() {
+//         return 'profile';
+//     });
 
-    Route::get('/photo', function() {
-        return 'photo';
-    });
-});
+//     Route::get('/photo', function() {
+//         return 'photo';
+//     });
+// });
 
-// ROUTE GROUP DALAM ROUTE GROUP
-Route::prefix('account')->group(function() {
-    Route::prefix('setting')->group(function() {
-        // definisikan route
-        Route::get('/change-password', function() {
-            return 'change-password';
-        });
+// // ROUTE GROUP DALAM ROUTE GROUP
+// Route::prefix('account')->group(function() {
+//     Route::prefix('setting')->group(function() {
+//         // definisikan route
+//         Route::get('/change-password', function() {
+//             return 'change-password';
+//         });
 
-        Route::get('/profile', function() {
-            return 'profile';
-        });
+//         Route::get('/profile', function() {
+//             return 'profile';
+//         });
 
-        Route::get('/photo', function() {
-            return 'photo';
-        });
-    });
+//         Route::get('/photo', function() {
+//             return 'photo';
+//         });
+//     });
 
-    Route::get('/follower', function() {
-        return 'Follower';
-    });
-});
+//     Route::get('/follower', function() {
+//         return 'Follower';
+//     });
+// });
 
-// ROUTE ALIAS NAME
-Route::get('redirect', function() {
-    return redirect()->route('homepage');
-});
+// // ROUTE ALIAS NAME
+// Route::get('redirect', function() {
+//     return redirect()->route('homepage');
+// });
 
-Route::get('home/landing', function() {
-    return 'Landing';
-})->name('homepage');
+// Route::get('home/landing', function() {
+//     return 'Landing';
+// })->name('homepage');
+
+// ROUTE RESOURCE
+Route::resource('article', 'ArticleController');
+
+// API RESOURCE
+Route::apiResource('article', 'ArticleController');
