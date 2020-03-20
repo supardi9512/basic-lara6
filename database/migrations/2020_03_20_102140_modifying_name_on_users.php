c2bd2618562e42a933aecebbf40530227ad379ca
Supardi <supardi9512@gmail.com>
2020-03-20 17:52:21 +0700

    memodifikasi kolom

---
+++ database/migrations/2020_03_20_102140_modifying_name_on_users.php
@@ -0,0 +1,33 @@
+<?php
+// php artisan make:migration modifying_name_on_users --table=users
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyingNameOnUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name', 100)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name', 50)->change();
+        });
+    }
+}
---
+++ database/migrations/2020_03_20_104838_rename_phone_on_users.php
@@ -0,0 +1,34 @@
+<?php
+// php artisan make:migration rename_phone_on_users --table=users
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenamePhoneOnUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('phone', 'phone_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('phone_number', 'phone');
+        });
+    }
+}
