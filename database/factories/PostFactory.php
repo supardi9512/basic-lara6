1581537e302ef770a1cac1bc0d6b60e5a3432491
Supardi <supardi9512@gmail.com>
2020-03-22 13:37:58 +0700

    seeder factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,14 @@
+<?php
+// php artisan make:factory PostFactory
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(5),
+        'content' => $faker->sentence(100),
+    ];
+});
--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -1,4 +1,5 @@
 <?php
+// php artisan make:seeder PostTableSeeder
 
 use Illuminate\Database\Seeder;
 use App\Post;
@@ -6,14 +7,6 @@ use Faker\Generator as Faker;
 
 class PostTableSeeder extends Seeder
 {
-    protected $post;
-    protected $faker;
-
-    public function __construct(Post $post, Faker $faker)
-    {
-        $this->post = $post;
-        $this->faker = $faker;
-    }
     /**
      * Run the database seeds.
      *
@@ -21,16 +14,6 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
-        // Post::create([
-        //     'title' => 'Title 1',
-        //     'content' => 'Example Content 1'
-        // ]);
-
-        foreach(range(1, 100) as $x) {
-            $this->post->create([
-                'title' => $this->faker->sentence(5),
-                'content' => $this->faker->sentence(30),
-            ]);
-        }
+        factory(Post::class, 150)->create();
     }
 }
